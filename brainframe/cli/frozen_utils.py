6f932649373e38439633ee13b71feb1120826c21
Tyler Compton <xaviosx@gmail.com>
2021-07-23 13:18:16 -0700

    Simplify resource loading logic

--- brainframe/cli/frozen_utils.py
+++ brainframe/cli/frozen_utils.py
@@ -18,10 +18,7 @@ def defaults_file_path() -> Path:
     :return: A path to the included defaults file
     :raises FileNotFoundError: If the defaults file is not present
     """
-    if is_frozen():
-        return _pyinstaller_tmp_path() / RELATIVE_DEFAULTS_FILE_PATH
-    else:
-        return _find_resource_location(RELATIVE_DEFAULTS_FILE_PATH)
+    return _get_resource_absolute_path(RELATIVE_DEFAULTS_FILE_PATH)
 
 
 def translations_path() -> Path:
@@ -29,25 +26,27 @@ def translations_path() -> Path:
     :return: A path to the folder with the translation files
     :raises FileNotFoundError: If the defaults file is not present
     """
-    if is_frozen():
-        return _pyinstaller_tmp_path() / RELATIVE_TRANSLATIONS_PATH
-    else:
-        return _find_resource_location(RELATIVE_TRANSLATIONS_PATH)
+    return _get_resource_absolute_path(RELATIVE_TRANSLATIONS_PATH)
 
 
-def _find_resource_location(target: Path) -> Path:
-    """Finds a resource given its path relative to the root of the project when the
-    application is being run from source.
+def _get_resource_absolute_path(target: Path) -> Path:
+    """Gets the absolute path of a resource.
 
     :param target: The relative path of the resource from the root of the project
-    :return: The absolute path to the resource
+    :return: The absolute path of the resource
+    :raises FileNotFoundError: If the resource cannot be found
     """
-    # Traverse up the file system from this file to find the root
-    for parent in Path(__file__).absolute().parents:
-        if (parent / target).exists():
-            return parent / target
+    if is_frozen():
+        path = _pyinstaller_tmp_path() / target
+        if not path.is_file():
+            raise FileNotFoundError()
+        return path
+    else:
+        for parent in Path(__file__).absolute().parents:
+            if (parent / target).exists():
+                return parent / target
 
-    raise FileNotFoundError()
+        raise FileNotFoundError()
 
 
 def _pyinstaller_tmp_path() -> Path:
